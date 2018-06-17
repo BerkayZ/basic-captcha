29bae848499ed4a996c6d2f6ed8de1bf0e604ed0
Berkay Zelyurt <zelyurtberkay@gmail.com>
2018-06-17 11:25:35 +0300

    Update post.php

--- post.php
+++ post.php
@@ -21,11 +21,11 @@
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-     <head>
-	     <title>Captcha Demo</title>
-	 </head>
-	 <!-- Example post file -->
-	 <body>
-	     <?php echo $info; ?>
-	 </body>
+   <head>
+      <title>Captcha Demo</title>
+   </head>
+   <!-- Example post file -->
+   <body>
+      <?php echo $info; ?>
+   </body>
 </html>
